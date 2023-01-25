12f4a86234941011161fb75bdad7df52b820b32a
lokesh <sailokesh7875@gmail.com>
2023-01-25 20:58:05 +0530

    2. Implemented UI

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
